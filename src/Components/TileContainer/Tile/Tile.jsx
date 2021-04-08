8b89f71f23b74b5565d853fa430b445acb409ca7
Devan Quinn <62554326+DevanQuinn@users.noreply.github.com>
2021-04-08 02:34:20 -0400

    fix html-parse error

--- src/Components/TileContainer/Tile/Tile.jsx
+++ src/Components/TileContainer/Tile/Tile.jsx
@@ -1,12 +1,11 @@
 import './Tile.css';
-import parse from 'html-react-parser';
 
 const Tile = ({ onClick, content }) => {
 	return (
 		<div onClick={() => onClick(content.index)}>
 			<div className='tile-bkg'>
 				<h1 className='tile-title'>{content.title}</h1>
-				<p className='tile-body'>{parse(content.body)}</p>
+				<p className='tile-body'>{content.body}</p>
 			</div>
 		</div>
 	);
